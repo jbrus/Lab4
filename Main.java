c1cd68dca67ee849c13b19b04e725fea46b8e31d
jbrus <32303638+jbrus@users.noreply.github.com>
2017-09-26 12:05:45 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This is Lab 4 version 2.");
+    }
+}
